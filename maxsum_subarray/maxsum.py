85be5662180d6f2fb1c8e539183cf6697b84116c
ishankaul1 <ishankaul2000@gmail.com>
2020-10-12 15:46:48 -0700

    changed some wording in maxsum

--- maxsum_subarray/maxsum.py
+++ maxsum_subarray/maxsum.py
@@ -24,11 +24,7 @@ def maxSub_Kadanes(array):
 #Splits array into left and right subarrays and solves
 #Then uses O(n) iterative approach to find maximum crossing subarray sum and combines
 
-def maxSub_DC_subopt(array):
-    result = maxSub_DC_subopt_helper(array)
-    return result
-
-def maxSub_DC_subopt_helper(array):
+def maxSub_DC_slow(array):
 
     #base case - array len 1
     if len(array) == 1:
@@ -39,8 +35,8 @@ def maxSub_DC_subopt_helper(array):
     L = array[:mid]
     R = array[mid:]
 
-    leftMaxSum = maxSub_DC_subopt_helper(L)
-    rightMaxSum = maxSub_DC_subopt_helper(R)
+    leftMaxSum = maxSub_DC_slow(L)
+    rightMaxSum = maxSub_DC_slow(R)
     crossMaxSum = maxCrossSum(L, R)
 
     #return solution
@@ -81,10 +77,10 @@ def maxCrossSum(L, R):
 #O(n) implementation of maximum subarray using divide and conquer aproach
 #Recursive function returns extra information (total array sum, max prefix, max suffix) to allow for computation of the cross sum in constant time
 
-def maxSub_DC_opt(array):
-    return maxSub_DC_opt_helper(array)[0]
+def maxSub_DC_fast(array):
+    return maxSub_DC_fast_helper(array)[0]
 
-def maxSub_DC_opt_helper(array):
+def maxSub_DC_fast_helper(array):
     #base case - array length 1
     if len(array) == 1:
         return array[0], array[0], array[0], array[0] #maxsum, totalsum, maxprefix, maxsuffix all = the value of the one element
@@ -93,8 +89,8 @@ def maxSub_DC_opt_helper(array):
     mid = math.floor(len(array)/2)
     L = array[:mid]
     R = array[mid:]
-    LmaxSum, LtotalSum, LmaxPrefix, LmaxSuffix = maxSub_DC_opt_helper(L)
-    RmaxSum, RtotalSum, RmaxPrefix, RmaxSuffix = maxSub_DC_opt_helper(R)
+    LmaxSum, LtotalSum, LmaxPrefix, LmaxSuffix = maxSub_DC_fast_helper(L)
+    RmaxSum, RtotalSum, RmaxPrefix, RmaxSuffix = maxSub_DC_fast_helper(R)
 
     #combine step - use extra information from both halves to compute all values for the combined array
     maxSum = max(LmaxSum, RmaxSum, LmaxSuffix + RmaxPrefix) #computation of cross array sum reduced to one addition due to extra information
@@ -111,8 +107,8 @@ def test1():
     array = [-1, 5, 8, -12, 3, 4, -10, 4, 4, -6,-2, -8, 1]
     print("Correct = 13")
     print("Kadane's algorithm: " + str(maxSub_Kadanes(array)))
-    print("Divide and Conquer (subopt): " + str(maxSub_DC_subopt(array)))
-    print("Divide and Conquer (opt): " + str(maxSub_DC_opt(array)))
+    print("Divide and Conquer (slow): " + str(maxSub_DC_slow(array)))
+    print("Divide and Conquer (fast): " + str(maxSub_DC_fast(array)))
     print()
 
 
@@ -120,16 +116,16 @@ def test2():
     array = [-1, -1, -1, -2, -1]
     print("Correct = -1")
     print("Kadane's algorithm: " + str(maxSub_Kadanes(array)))
-    print("Divide and Conquer (subopt): " + str(maxSub_DC_subopt(array)))
-    print("Divide and Conquer (opt): " + str(maxSub_DC_opt(array)))
+    print("Divide and Conquer (slow): " + str(maxSub_DC_slow(array)))
+    print("Divide and Conquer (fast): " + str(maxSub_DC_fast(array)))
     print()
 
 def test3():
     array = [1, 2, 3, 4, 5, 6, 7, 8]
     print("Correct = " + str(int(8 * 9 / 2)))
     print("Kadane's algorithm: " + str(maxSub_Kadanes(array)))
-    print("Divide and Conquer (subopt): " + str(maxSub_DC_subopt(array)))
-    print("Divide and Conquer (opt): " + str(maxSub_DC_opt(array)))
+    print("Divide and Conquer (slow): " + str(maxSub_DC_slow(array)))
+    print("Divide and Conquer (fast): " + str(maxSub_DC_fast(array)))
     print()
 
 
@@ -137,24 +133,24 @@ def test4():
     array = [-8, -10, 1, -4]
     print("Correct = 1")
     print("Kadane's algorithm: " + str(maxSub_Kadanes(array)))
-    print("Divide and Conquer (subopt): " + str(maxSub_DC_subopt(array)))
-    print("Divide and Conquer (opt): " + str(maxSub_DC_opt(array)))
+    print("Divide and Conquer (slow): " + str(maxSub_DC_slow(array)))
+    print("Divide and Conquer (fast): " + str(maxSub_DC_fast(array)))
     print()
 
 def test5():
     array = [-1]
     print("Correct = -1")
     print("Kadane's algorithm: " + str(maxSub_Kadanes(array)))
-    print("Divide and Conquer (subopt): " + str(maxSub_DC_subopt(array)))
-    print("Divide and Conquer (opt): " + str(maxSub_DC_opt(array)))
+    print("Divide and Conquer (slow): " + str(maxSub_DC_slow(array)))
+    print("Divide and Conquer (fast): " + str(maxSub_DC_fast(array)))
     print()
 
 def test6():
     array = [54, -100, 12, 18, -44, 37, -500, 200, 42, 3, -100, -1000, 220]
     print("Correct = 245")
     print("Kadane's implementation: " + str(maxSub_Kadanes(array)))
-    print("Divide and Conquer (subopt): " + str(maxSub_DC_subopt(array)))
-    print("Divide and Conquer (opt): " + str(maxSub_DC_opt(array)))
+    print("Divide and Conquer (slow): " + str(maxSub_DC_slow(array)))
+    print("Divide and Conquer (fast): " + str(maxSub_DC_fast(array)))
     print()
 
 def main():
